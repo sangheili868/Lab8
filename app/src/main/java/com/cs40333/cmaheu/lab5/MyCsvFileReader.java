04597e46a220835a9adcab16a5999cb13c3d8a91
sangheili868 <chrisii868@gmail.com>
2017-03-05 15:54:25 -0500

    Initial commit

---
+++ app/src/main/java/com/cs40333/cmaheu/lab5/MyCsvFileReader.java
@@ -0,0 +1,48 @@
+package com.cs40333.cmaheu.lab5;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class MyCsvFileReader {
+
+    Context context;
+    public MyCsvFileReader(Context applicationContext) {
+        this.context = applicationContext;
+    }
+
+    public ArrayList<String[]> readCsvFile(int fileresource) {
+        ArrayList<String[]> games = new ArrayList<>();
+        InputStream fin = null;
+        InputStreamReader isr = null;
+        BufferedReader reader = null;
+        try {
+            fin = context.getResources().openRawResource(fileresource);
+            isr = new InputStreamReader(fin);
+            reader = new BufferedReader(isr);
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] teamInfo = line.split(";");
+                games.add(teamInfo);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (isr != null)
+                    isr.close();
+                if (fin != null)
+                    fin.close();
+                if (reader != null)
+                    reader.close();
+            } catch (IOException ex) {
+                ex.getMessage();
+            }
+        }
+        return games;
+    }
+}
---
+++ app/src/main/java/com/cs40333/cmaheu/lab5/Team.java
@@ -0,0 +1,102 @@
+package com.cs40333.cmaheu.lab5;
+
+import java.io.Serializable;
+
+public class Team implements Serializable {
+    String shortDate;
+    String longDate;
+    String location;
+    String logo;
+    String placeName;
+    String teamName;
+    String record;
+    String score;
+    String timeleft;
+
+    Team (String _shortDate, String _longDate, String _location, String _logo,
+          String _placeName, String _teamName, String _record, String _score,
+          String _timeleft) {
+        setShortDate(_shortDate);
+        setLongDate(_longDate);
+        setLocation(_location);
+        setLogo(_logo);
+        setPlaceName(_placeName);
+        setTeamName(_teamName);
+        setRecord(_record);
+        setScore(_score);
+        setTimeleft(_timeleft);
+
+    }
+
+    public String getShortDate() {
+        return shortDate;
+    }
+
+    public String getLongDate() {
+        return longDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getRecord() {
+        return record;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public String getTimeleft() {
+        return timeleft;
+    }
+
+    public void setShortDate(String shortDate) {
+        this.shortDate = shortDate;
+    }
+
+    public void setLongDate(String longDate) {
+        this.longDate = longDate;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public void setTimeleft(String timeleft) {
+        this.timeleft = timeleft;
+    }
+}
